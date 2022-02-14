0b9349f138436e0d83935f2bbc8f4f3a46ed528e
Christoph Ungricht <christoph.ungricht@outlook.com>
2022-02-15 00:20:10 +0100

    print board

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::fmt;
+use std::fmt::{Formatter, write};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 enum Color {
@@ -17,119 +18,140 @@ enum Name {
     Queen,
 }
 
+impl fmt::Display for Name {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut res = String::new();
+        match self {
+            Name::Pawn => write!(f, "P"),
+            Name::Bishop => write!(f, "B"),
+            Name::Knight => write!(f, "H"),
+            Name::King => write!(f, "K"),
+            Name::Rook => write!(f, "R"),
+            Name::Queen => write!(f, "Q")
+        }
+    }
+}
+
 type Position = (char, char);
 
 #[derive(Debug, Clone)]
 struct Piece {
     name: Name,
     color: Color,
-    position: Position,
     value: f32,
 }
 
 #[derive(Debug, Clone)]
 struct Board {
-    pieces: HashMap<Color, Vec<Piece>>,
+    pieces: HashMap<Position, Option<Piece>>,
     captured: HashMap<Color, Vec<Piece>>,
 }
 
 impl Board {
     fn new() -> Board {
         // White pieces
-        let pawn_w1 = Piece { name: Name::Pawn, color: Color::White, position: ('a', '2'), value: 1.0 };
-        let pawn_w2 = Piece { position: ('b', '2'), ..pawn_w1.clone() };
-        let pawn_w3 = Piece { position: ('b', '2'), ..pawn_w1.clone() };
-        let pawn_w4 = Piece { position: ('c', '2'), ..pawn_w1.clone() };
-        let pawn_w5 = Piece { position: ('d', '2'), ..pawn_w1.clone() };
-        let pawn_w6 = Piece { position: ('e', '2'), ..pawn_w1.clone() };
-        let pawn_w7 = Piece { position: ('f', '2'), ..pawn_w1.clone() };
-        let pawn_w8 = Piece { position: ('g', '2'), ..pawn_w1.clone() };
+        let pawn_w = Piece { name: Name::Pawn, color: Color::White, value: 1.0 };
 
-        let knight_w1 = Piece { name: Name::Knight, color: Color::White, position: ('b', '1'), value: 3.0 };
-        let knight_w2 = Piece { position: ('g', '1'), ..knight_w1.clone() };
+        let knight_w = Piece { name: Name::Knight, color: Color::White, value: 3.0 };
 
-        let bishop_w1 = Piece { name: Name::Bishop, color: Color::White, position: ('c', '1'), value: 3.0 };
-        let bishop_w2 = Piece { position: ('f', '1'), ..bishop_w1.clone() };
+        let bishop_w = Piece { name: Name::Bishop, color: Color::White, value: 3.0 };
 
-        let rook_w1 = Piece { name: Name::Rook, color: Color::White, position: ('a', '1'), value: 5.0 };
-        let rook_w2 = Piece { position: ('h', '1'), ..rook_w1.clone() };
+        let rook_w = Piece { name: Name::Rook, color: Color::White, value: 5.0 };
 
-        let queen_w = Piece { name: Name::Queen, color: Color::White, position: ('d', '1'), value: 8.0 };
+        let queen_w = Piece { name: Name::Queen, color: Color::White, value: 8.0 };
 
-        let king_w = Piece { name: Name::King, color: Color::White, position: ('e', '1'), value: 3.5 };
+        let king_w = Piece { name: Name::King, color: Color::White, value: 3.5 };
 
         // Black pieces
-        let pawn_b1 = Piece { name: Name::Pawn, color: Color::Black, position: ('a', '2'), value: 1.0 };
-        let pawn_b2 = Piece { position: ('b', '2'), ..pawn_b1.clone() };
-        let pawn_b3 = Piece { position: ('b', '2'), ..pawn_b1.clone() };
-        let pawn_b4 = Piece { position: ('c', '2'), ..pawn_b1.clone() };
-        let pawn_b5 = Piece { position: ('d', '2'), ..pawn_b1.clone() };
-        let pawn_b6 = Piece { position: ('e', '2'), ..pawn_b1.clone() };
-        let pawn_b7 = Piece { position: ('f', '2'), ..pawn_b1.clone() };
-        let pawn_b8 = Piece { position: ('g', '2'), ..pawn_b1.clone() };
+        let pawn_b = Piece { name: Name::Pawn, color: Color::Black, value: 1.0 };
 
-        let knight_b1 = Piece { name: Name::Knight, color: Color::Black, position: ('b', '1'), value: 3.0 };
-        let knight_b2 = Piece { position: ('g', '1'), ..knight_b1.clone() };
+        let knight_b = Piece { name: Name::Knight, color: Color::Black, value: 3.0 };
 
-        let bishop_b1 = Piece { name: Name::Bishop, color: Color::Black, position: ('c', '1'), value: 3.0 };
-        let bishop_b2 = Piece { position: ('f', '1'), ..bishop_b1.clone() };
+        let bishop_b = Piece { name: Name::Bishop, color: Color::Black, value: 3.0 };
 
-        let rook_b1 = Piece { name: Name::Rook, color: Color::Black, position: ('a', '1'), value: 5.0 };
-        let rook_b2 = Piece { position: ('h', '1'), ..rook_b1.clone() };
+        let rook_b = Piece { name: Name::Rook, color: Color::Black, value: 5.0 };
 
-        let queen_b = Piece { name: Name::Queen, color: Color::Black, position: ('d', '1'), value: 8.0 };
+        let queen_b = Piece { name: Name::Queen, color: Color::Black, value: 8.0 };
 
-        let king_b = Piece { name: Name::King, color: Color::Black, position: ('e', '1'), value: 3.5 };
+        let king_b = Piece { name: Name::King, color: Color::Black, value: 3.5 };
 
-        let pieces = HashMap::from(
-            [
-                (Color::White,
-                 vec![
-                     pawn_w1,
-                     pawn_w2,
-                     pawn_w3,
-                     pawn_w4,
-                     pawn_w5,
-                     pawn_w6,
-                     pawn_w7,
-                     pawn_w8,
-                     bishop_w1,
-                     bishop_w2,
-                     knight_w1,
-                     knight_w2,
-                     rook_w1,
-                     rook_w2,
-                     queen_w,
-                     king_w,
-                 ]),
-                (Color::Black,
-                 vec![
-                     pawn_b1,
-                     pawn_b2,
-                     pawn_b3,
-                     pawn_b4,
-                     pawn_b5,
-                     pawn_b6,
-                     pawn_b7,
-                     pawn_b8,
-                     bishop_b1,
-                     bishop_b2,
-                     knight_b1,
-                     knight_b2,
-                     rook_b1,
-                     rook_b2,
-                     queen_b,
-                     king_b,
-                 ]),
-            ]
-        );
+        let mut pieces: HashMap<Position, Option<Piece>> = HashMap::new();
+        for x in 'a'..='h' {
+            for y in '1'..='8' {
+                if y == '2' {
+                    pieces.insert((x.clone(), y), Some(pawn_w.clone()));
+                } else if y == '7' {
+                    pieces.insert((x.clone(), y), Some(pawn_b.clone()));
+                } else if y == '1' && (x == 'a' || x == 'h') {
+                    pieces.insert((x.clone(), y), Some(rook_w.clone()));
+                } else if y == '8' && (x == 'a' || x == 'h') {
+                    pieces.insert((x.clone(), y), Some(rook_b.clone()));
+                } else if y == '1' && (x == 'b' || x == 'g') {
+                    pieces.insert((x.clone(), y), Some(knight_w.clone()));
+                } else if y == '8' && (x == 'b' || x == 'g') {
+                    pieces.insert((x.clone(), y), Some(knight_b.clone()));
+                } else if y == '1' && (x == 'c' || x == 'f') {
+                    pieces.insert((x.clone(), y), Some(bishop_w.clone()));
+                } else if y == '8' && (x == 'c' || x == 'f') {
+                    pieces.insert((x.clone(), y), Some(bishop_b.clone()));
+                } else if y == '1' && x == 'd' {
+                    pieces.insert((x.clone(), y), Some(queen_w.clone()));
+                } else if y == '8' && x == 'd' {
+                    pieces.insert((x.clone(), y), Some(queen_b.clone()));
+                } else if y == '1' && x == 'e' {
+                    pieces.insert((x.clone(), y), Some(king_w.clone()));
+                } else if y == '8' && x == 'e' {
+                    pieces.insert((x.clone(), y), Some(king_b.clone()));
+                } else {
+                    pieces.insert((x.clone(), y), None);
+                }
+            }
+        }
         let captured: HashMap<Color, Vec<Piece>> = HashMap::new();
         Board { pieces, captured }
     }
 }
 
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut res = String::new();
+        for _ in 1..=8 {
+            res.push_str("\t-");
+        }
+        res.push_str("\n");
+
+        for y in ('1'..='8').rev() {
+            res.push_str(format!("{}|\t",y).as_str());
+            for x in 'a'..='h' {
+                match &self.pieces[&(x, y)] {
+                    None => res.push_str("  |\t"),
+                    Some(piece) => {
+                        let pre_fix = if piece.color == Color::White {
+                            String::from("W")
+                        } else {
+                            String::from("B")
+                        };
+                        res.push_str(format!("{}{}|\t", pre_fix, piece.name).as_str())
+                    }
+                }
+            }
+            res.push_str(format!("\n").as_str());
+
+            for _ in 1..=8 {
+                res.push_str("\t-");
+            }
+            res.push_str("\n");
+
+        }
+        for x in 'a'..='h' {
+            res.push_str(format!("\t{}",x).as_str());
+        }
+        res.push_str("\n");
+        write!(f, "{}", res)
+    }
+}
 
 fn main() {
     let board = Board::new();
-    println!("{:?}", board);
+    println!("{}", board);
 }
