49df62328c33b0b1c98287e36286dd71a2adc8a0
Christoph Ungricht <christoph.ungricht@outlook.com>
2023-12-02 15:06:09 +0100

    feat: Added static game of live.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- wasm/index.js
+++ wasm/index.js
@@ -1,3 +1,20 @@
-import * as wasm from "rusty-chess-wasm"
+import { Universe } from "rusty-chess-wasm";
 
-wasm.greet()
+const pre = document.getElementById("rusty-chess-wasm-canvas");
+const width = 64;
+const height = 32;
+
+const universe = Universe.new(width, height);
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function renderLoop() {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  sleep(100).then(() => requestAnimationFrame(renderLoop));
+}
+
+requestAnimationFrame(renderLoop);
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,125 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = match cell {
+                    Cell::Dead => '◻',
+                    Cell::Alive => '◼',
+                };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        debug_assert_ne!(self.height, 0);
+        debug_assert_ne!(self.width, 0);
+
+        for row_diff in [self.height - 1, 0, 1] {
+            for col_diff in [self.width - 1, 0, 1] {
+                if row_diff == 0 && col_diff == 0 {
+                    continue;
+                }
+                let new_row = ((row as u64 + row_diff as u64) % self.height as u64) as u32;
+                let new_col = ((column as u64 + col_diff as u64) % self.width as u64) as u32;
+                let neighbor_index = self.get_index(new_row, new_col);
+                count += self.cells[neighbor_index] as u8;
+            }
+        }
+        count
+    }
+}
+/// Public methods, exported to JavaScript.
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbours
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new(width: u32, height: u32) -> Universe {
+        assert_ne!(width, 0);
+        assert_ne!(height, 0);
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,6 +127,6 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, Rusty-Chess!");
+pub fn greet(who: &str) {
+    alert(&format!("Hello, {who}!"));
 }
